a8cd4d804259dd9f94f7bd701ddefaaf4596a327
wotupset <wotupset@gmail.com>
2014-05-15 03:20:42 +0800

    0320

--- 01/safemode/index.php
+++ 01/safemode/index.php
@@ -1,162 +1,156 @@
 <?php
-header("content-Type: text/html; charset=utf-8"); //語言強制
-$query_string=$_SERVER['QUERY_STRING'];
-$phpself=basename($_SERVER["SCRIPT_FILENAME"]);//被執行的文件檔名
-$phphost=$_SERVER["SERVER_NAME"];
 date_default_timezone_set("Asia/Taipei");//時區設定 Etc/GMT+8
-$time = time();
-//$tim = $time.substr(microtime(),2,3);
-//$tim = microtime(true);
-$ver="v140417.1646";
-$ver_md5=md5(sha1($ver));//依版本號加密成MD5
-$ver_color="#".substr($ver_md5,-6);//版本號的顏色
-$md5_file=md5_file("./".$phpself."") or die("[x]md5_file");
+$time=time();
+$ym=date("ym",$time);
+$query_string=$_SERVER['QUERY_STRING'];
+$phplink="http://".$_SERVER["SERVER_NAME"].$_SERVER["SCRIPT_NAME"]."";
+$phpdir="http://".$_SERVER["SERVER_NAME"]."".$_SERVER["PHP_SELF"]."";
+$phpdir=substr($phpdir,0,strrpos($phpdir,"/")+1); //根目錄
+$phpself=basename($_SERVER["SCRIPT_FILENAME"]);//被執行的文件檔名
 //**********
-$url="./";
+if(preg_match("/^([0-9]+)/",$query_string,$match)){
+	$pg_n=$match[1];//分頁的頁數
+}
+unset($match);
+if(!$pg_n){$pg_n=0;}//沒指定就是0
+
+//**********
+$dir_mth="./"; //掃描根目錄
+$url=$dir_mth;
 $handle=opendir($url); 
 $cc = 0;
+$FFF_arr=array();
 while(($file = readdir($handle))!==false) { 
-	if(1) { 
-		$tmp[0][$cc] = $file; 
-		if($file=="."||$file == ".."){
-			$tmp[1][$cc] = "0";
-			$tmp[2][$cc] = "y";//系統功能的資料夾
-		}else{
-			if(is_dir($file)){
-				$tmp[1][$cc] = "0";
-				$tmp[2][$cc] = "y";
-			}else{
-				$tmp[1][$cc] = filesize($file);//檔案大小
-				$tmp[2][$cc] = "n";
-			}
-		}
-		//$tmp[$cc] = substr($file,0,strpos($file,"."));
-	} 
+	$chk=0;
+	if(preg_match("/\.jpg$/i",$file)){$chk=1;}//只要圖
+	if(preg_match("/\.png$/i",$file)){$chk=1;}//只要圖
+	if(preg_match("/\.gif$/i",$file)){$chk=1;}//只要圖
+	if($chk==1){$FFF_arr[]=$file;}
 	$cc = $cc + 1;
 } 
 closedir($handle); 
+sort($FFF_arr);//排序 舊的在前
 //**********
-//排序 //rsort($tmp);
-$array_lowercase = array_map('strtolower', $tmp[0]);
-array_multisort($array_lowercase, SORT_ASC, SORT_STRING, $tmp[0],$tmp[1],$tmp[2]);
-//array_multisort($tmp[0],$tmp[2]);
-//**********
+//列出左側分頁
+$arr_count=count($FFF_arr);//計算數量
+$pg_max=floor($arr_count/10);
+if($arr_count%10 != 0){$pg_max=$pg_max+1;} //有餘數再加一
+if($pg_n>$pg_max){$pg_n=$pg_max;}//指定頁數太大 就更換成max值
 
-$httphead = <<<EOT
+$cc=1;$pg_html='';$FFF='';
+for($i=0;$i<$pg_max;$i++){
+	if($i == $pg_n){$FFF="&nbsp;<span id='menu2_pi'>&#9619;&#9618;&#9617;</span>";}else{$FFF='';}
+	$pg_html.="<a href='".$phpself."?".$i."'>".$i."</a>".$FFF;
+	$pg_html.="<br/>\n";
+	$cc=$cc+1;
+
+}
+//**********
+//列出右側圖片
+$cc=1;$pic='';
+foreach($FFF_arr as $k => $v ){
+	//if(){continue;}
+	if( ($k>= ($pg_n)*10 ) && ($k< ($pg_n+1)*10 ) ){
+		//$pic_src=$phpdir.$dir_mth.$v;
+		$pic_src=$dir_mth.$v;
+		//$pic_size=filesize($pic_src);
+		$fn=$v;
+		$fn_a=substr($fn,0,strrpos($fn,".")); //主檔名
+		$fn_b=strrpos($fn,".")+1-strlen($fn);
+		$fn_b=substr($fn,$fn_b); //副檔名
+		$pic.= $k;
+		if(strtolower($fn_b) == "gif"){$pic.="GIF";}
+		$pic.= "<br/>\n";
+		$pic.= "<a href='".$pic_src."' target='_blank'><img src='".$pic_src."'/></a>";
+		$pic.= "<br/>\n";
+	}
+	$cc=$cc+1;
+}
+
+$htmlbody=<<<EOT
+<div id="menu2" style="z-index:9;position: fixed; margin: 0px; padding: 0px; left: 0px; top: 0px; color: #cc0000; background-color: #ffffff; border-right: 1px black solid; overflow: auto; width: 125px;height:90%;">
+	<div style="padding: 10px;">
+		$pg_html
+	</div>
+</div>
+<div id="menu3" style="z-index:8;position: fixed; margin-bottom: 0px; padding: 5px; width: 100%; left: 0px; bottom: 0px; color: #cc0000; background-color: #ffffee; border-top: 1px black solid; ">
+	<div style="font-size: 12px;margin-bottom:5px;">
+		<a href='index2.php'>資料夾模式</a> 分頁 $pg_n <br/>
+	</div>
+</div>
+<div id="right_content" style="margin: auto auto 50px 160px;">
+	$pic
+</div>
+EOT;
+
+echo htmlhead();
+echo $htmlbody;
+echo htmlend();
+
+
+function htmlhead(){
+$x=<<<EOT
 <html><head>
-<title>$phphost</title>
+<title>$ymdhis</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <META http-equiv="Content-Script-Type" content="text/javascript">
 <META http-equiv="Content-Style-Type" content="text/css">
-<META NAME='ROBOTS' CONTENT='noINDEX, FOLLOW'>
-<STYLE TYPE="text/css"><!--
-/*
-border-width:0px 0px 0px 10px;
-border-color:pink;
-border-style:solid;
-border-left:12px solid green;
-position:relative;left:-12px;top:0px;z-index:2;
-td {white-space: nowrap; overflow: hidden; }
-position:relative;right:0px;top:0px;z-index:2;
-border-width:0px 20px 0px 0px;
-border-color:pink;
-border-style:solid;
-*/
-body {}
-table {
-font-family:"細明體",'MingLiU';
-font-size:16px;
-border-collapse:collapse;
-border-spacing:0;
-table-layout: fixed;
+<META HTTP-EQUIV="EXPIRES" CONTENT="Thu, 15 Jan 2009 05:12:01 GMT">
+<META NAME="ROBOTS" CONTENT="INDEX,FOLLOW">
+<STYLE TYPE="text/css">
+body2 { font-family:"細明體",'MingLiU'; }
+img {
+height:auto; width:auto; 
+min-width:20px; min-height:20px;
+max-width:250px; max-height:250px;
+border:1px solid blue;
 }
-a {text-decoration:none;}
-a:hover {text-decoration:underline;}
-.td_left {color:#eeaa88;}
-tr:hover{background-color:#F0E0D6;
-}
-tr:hover .span_left {
-color:#000000;
-border-width:0px 20px 0px 0px;
-border-color:pink;
-border-style:solid;
-}
-tr:hover .span2_left {
-position:relative;right:-20px;top:0px;z-index:2;
-}
-tr:hover .span_right {
-visibility:visible;
-}
-.td_left {
-white-space: nowrap; 
-overflow: hidden; 
-width: 80px;
-}
-.td_right{
-white-space: nowrap; 
-overflow: hidden; 
-width: 420px;
-}
+</STYLE>
+<script src="http://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+<script language="Javascript">
 
---></STYLE>
+////
+function reset() { 
+	//var tmp1 = tmp2 = tmp3 = 0;alert(tmp2);
+	//(tmp3.clientHeight - 200)
+	//$("#menu2").height =  "400px"; //選單高度修正
+	//document.getElementById("menu2").style.height = "200px";
+	var tmp4 = $("#menu3").outerHeight(true);//下方選單高度// set to true, the margin (top and bottom) is also included.
+	//alert(tmp4);
+	var tmp3 = $(document.body)[0].clientHeight -tmp4;//計算左側選單底部要縮多少高度
+	$("#menu2").height(tmp3);
+	var tmp5 = tmp4 +20;//計算右側內容底部要墊多少高度
+	$("#right_content").css("margin-bottom",tmp5+"px");
+	////
+}
+////
+$(document).ready(function(){
+	reset();
+	$(window).resize(function(){reset();});
+	//window.onresize = reset();
+	//$("#menu2").scrollTop(tmp);
+	//$("#menu2").height()
+////
+	var tmp1 = $("#menu2").outerHeight(true);//左側選單高度 //height()
+	var tmp2 = $("#menu2_pi").position().top - (tmp1/2); //計算左側選單捲軸要停留的位置
+	//$("#menu2").animate({scrollTop: tmp2 },1000,"swing");//jq動畫方式移動捲軸
+	$("#menu2").scrollTop(tmp2);
+});
+</script>
 </head>
-<body bgcolor="#FFFFEE" text="#800000" link="#0000EE" vlink="#003333">
+<body bgcolor="#FFFFEE" text="#800000" link="#0000EE" vlink="#0000ee">
 
 EOT;
+$x="\n".$x."\n";
+return $x;
+}
 
-$httpend = <<<EOT
+function htmlend(){
+$x=<<<EOT
+
 </body></html>
 EOT;
-
-
-$httpbody="";//echo
-$date_now=date("y/m/d H:i:s", $time);
-$ver_info= <<<EOT
-<blockquote><pre><span style='color:$ver_color;'>$ver</span> $md5_file</pre></blockquote>
-EOT;
-$httpbody.="\n";
-$line = count($tmp[0]);
-if($line>=1000){$line=1000;}else{$line=$line;}
-$tmp_str=<<<EOT
-
-$ver_info
-<table style="width: 500px">
-<thead>
-<tr>
-<th style='text-align: right;width: 80px;'>size</th>
-<th style='text-align: left;width: 420px;'>name</th>
-</tr>
-</thead>
-EOT;
-$httpbody.=$tmp_str;
-$httpbody.="\n<tbody>\n";
-for($i = 0; $i < $line; $i++){//從頭
-	$tmp_0_i=$tmp[0][$i];
-	$tmp_1_i=$tmp[1][$i];
-	$tmp_2_i=$tmp[2][$i];
-	
-	if($tmp[2][$i]=="y"){//是資料夾
-		$tmp_0_i_dirmark="◆";
-	}else{//不是資料夾
-		$tmp_0_i_dirmark="";
-	}
-$httpbody.=<<<EOT
-<tr>
-<td class="td_left" style='text-align: right;'><span class="span_left"><span class="span2_left">$tmp_1_i</span></span></td>
-<td class="td_right" style='text-align: left;' ><span class="span_right"><a href='./$tmp_0_i'>$tmp_0_i</a>$tmp_0_i_dirmark</span></td>
-</tr>
-
-EOT;
-}//
-$cc2=$cc-2;
-$httpbody.="</tbody>\n</table>\n";
-$httpbody.="\n<blockquote><pre>$date_now $cc2</pre></blockquote>\n";
-
-$httpbody= "\n".$httpbody."\n";
-echo $httphead."\n" ;
-echo $httpbody."\n" ;
-echo $httpend."\n" ;
-
-
-
+$x="\n".$x."\n";
+return $x;
+}
 ?>
